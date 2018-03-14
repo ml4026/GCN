b2f8636aef7964930788d47ea4c092cfdb7f6c04
Minhui Li <33358143+ml4026@users.noreply.github.com>
2018-03-14 10:30:38 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,265 @@
+from __future__ import print_function
+
+from keras.layers import Input##, Dropout
+from keras.models import Model
+from keras.optimizers import Adam
+from keras import activations, initializers, constraints, regularizers
+from keras.engine import Layer
+import keras.backend as K
+
+import time
+import scipy.sparse as sp
+import numpy as np
+
+class GraphConvolution(Layer):
+    #Basic graph convolution layer as in https://arxiv.org/abs/1609.02907
+    def __init__(self, units, support=1,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super(GraphConvolution, self).__init__(**kwargs)
+        
+        self.units = units
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.supports_masking = True
+        self.support = support
+
+    def compute_output_shape(self, input_shapes):
+        features_shape = input_shapes[0]
+        output_shape = (features_shape[0], self.units)
+        return output_shape  # (batch_size, output_dim)
+    
+    def build(self, input_shapes):
+        features_shape = input_shapes[0]
+        assert len(features_shape) == 2
+        input_dim = features_shape[1]
+        
+        self.kernel = self.add_weight(shape=(input_dim, self.units),
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+            self.built = True
+
+    def call(self, inputs, mask=None):
+        features = inputs[0]
+        basis = inputs[1:]
+
+        supports = K.concatenate([K.dot(basis[0], features)], axis=1) ##Convert list to tensor
+        output = K.dot(supports, self.kernel)
+        
+        if self.bias:
+            output += self.bias
+        return self.activation(output)
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'support': self.support,
+                  'activation': activations.serialize(self.activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer': initializers.serialize(self.kernel_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint)}
+        base_config = super(GraphConvolution, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+def load_data():
+    #Load citation network dataset (cora only for now)
+    print('Loading dataset...')
+    
+    idx_features_labels = np.genfromtxt("data/cora.content", dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_features_labels[:, -1])
+    
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("data/cora.cites", dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+                     
+                     # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], edges.shape[0], features.shape[1]))
+                     
+    return features.todense(), adj, labels
+
+
+def normalize_adj(adj, symmetric=True):
+    if symmetric:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
+        a_norm = adj.dot(d).transpose().dot(d).tocsr()
+    else:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -1).flatten(), 0)
+        a_norm = d.dot(adj).tocsr()
+    return a_norm
+
+
+def preprocess_adj(adj, symmetric=True):
+    adj = adj + sp.eye(adj.shape[0])
+    adj = normalize_adj(adj, symmetric)
+    return adj
+
+
+def sample_mask(idx, l):
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=np.bool)
+
+
+def get_splits(y):
+    idx_train = range(1500)
+    idx_val = range(1500, 1700)
+    idx_test = range(1700, 2700)
+    y_train = np.zeros(y.shape, dtype=np.int32)
+    y_val = np.zeros(y.shape, dtype=np.int32)
+    y_test = np.zeros(y.shape, dtype=np.int32)
+    y_train[idx_train] = y[idx_train]
+    y_val[idx_val] = y[idx_val]
+    y_test[idx_test] = y[idx_test]
+    train_mask = sample_mask(idx_train, y.shape[0])
+    return y_train, y_val, y_test, idx_train, idx_val, idx_test, train_mask
+
+def categorical_crossentropy(preds, labels):
+    return np.mean(-np.log(np.extract(labels, preds)))
+
+def accuracy(preds, labels):
+    return np.mean(np.equal(np.argmax(labels, 1), np.argmax(preds, 1)))
+
+def evaluate_preds(preds, labels, indices):
+    
+    split_loss = list()
+    split_acc = list()
+    
+    for y_split, idx_split in zip(labels, indices):
+        split_loss.append(categorical_crossentropy(preds[idx_split], y_split[idx_split]))
+        split_acc.append(accuracy(preds[idx_split], y_split[idx_split]))
+    
+    return split_loss, split_acc
+
+# Define parameters
+FILTER = 'localpool'  # 'chebyshev'
+MAX_DEGREE = 2  # maximum polynomial degree
+SYM_NORM = True  # symmetric (True) vs. left-only (False) normalization
+NB_EPOCH = 200
+PATIENCE = 10  # early stopping patience
+
+# Get data
+X, A, y = load_data() ## X = features, A = adjacency matrix, y = labels
+y_train, y_val, y_test, idx_train, idx_val, idx_test, train_mask = get_splits(y)
+
+# Normalize X
+X /= X.sum(1).reshape(-1, 1) ## Normalization, reshape here reshapes X.sum to a column list. -1 means unknown
+
+#Local pooling filters (see 'renormalization trick' in Kipf & Welling, arXiv 2016)
+print('Using local pooling filters...')
+A_ = preprocess_adj(A, SYM_NORM) ## Normalize adjacency matrix
+graph = [X, A_] ## Construct graph by putting nodes and edges together. Both of them are preprocesed.
+G = [Input(shape=(None, None), batch_shape=(None, None), sparse=True)]
+
+X_in = Input(shape=(X.shape[1],)) ## Input Layer?? X.shape[1] is the number of features
+
+# Define model architecture
+# NOTE: We pass arguments for graph convolutional layers as a list of tensors.
+# This is somewhat hacky, more elegant options would require rewriting the Layer base class.
+#H = Dropout(0.5)(X_in)
+H = GraphConvolution(16, activation='relu', kernel_regularizer=regularizers.l2(5e-4))([X_in]+G)#([H]+G)
+#H = Dropout(0.5)(H)
+#H = GraphConvolution(16, activation='relu', kernel_regularizer=regularizers.l2(5e-4))([H]+G)
+#H = Dropout(0.5)(H)
+Y = GraphConvolution(y.shape[1], activation='softmax')([H]+G)
+
+'''
+    GraphConvolution
+      units
+      support
+      activation
+      use_bias
+      kernel_initializer
+'''
+# Compile model
+model = Model(inputs=[X_in]+G, outputs=Y)
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.01))
+
+# Helper variables for main training loop
+wait = 0
+preds = None
+best_val_loss = 99999
+
+# Fit
+for epoch in range(1, NB_EPOCH+1):
+
+    # Log wall-clock time
+    t = time.time()
+
+    # Single training iteration (we mask nodes without labels for loss calculation)
+    model.fit(graph, y_train, sample_weight=train_mask,
+              batch_size=A.shape[0], epochs=1, shuffle=False, verbose=0) ## Batch
+
+    # Predict on full dataset
+    preds = model.predict(graph, batch_size=A.shape[0])
+
+    # Train / validation scores
+    train_val_loss, train_val_acc = evaluate_preds(preds, [y_train, y_val],
+                                                   [idx_train, idx_val])
+    print("Epoch: {:04d}".format(epoch),
+          "train_loss= {:.4f}".format(train_val_loss[0]),
+          "train_acc= {:.4f}".format(train_val_acc[0]),
+          "val_loss= {:.4f}".format(train_val_loss[1]),
+          "val_acc= {:.4f}".format(train_val_acc[1]),
+          "time= {:.4f}".format(time.time() - t))
+
+    # Early stopping
+    if train_val_loss[1] < best_val_loss:
+        best_val_loss = train_val_loss[1]
+        wait = 0
+    else:
+        if wait >= PATIENCE:
+            print('Epoch {}: early stopping'.format(epoch))
+            break
+        wait += 1
+
+# Testing
+test_loss, test_acc = evaluate_preds(preds, [y_test], [idx_test])
+print("Test set results:",
+      "loss= {:.4f}".format(test_loss[0]),
+      "accuracy= {:.4f}".format(test_acc[0]))
